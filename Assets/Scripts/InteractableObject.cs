f0ab616ae65305cfbfffae0ec797bd9a5ad7eef1
Ryuzaki <ojizaki@gmail.com>
2016-04-03 15:57:55 +0200

    interaction scripts and shaders

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class InteractableObject : MonoBehaviour {
+
+	public enum InteractableType {
+		Grab,
+		See,
+		Push
+	}
+
+	public InteractableType interactableType;
+
+	public virtual void DoGrab() {}
+
+	public virtual void DoSee() {}
+
+	public virtual void DoPush() {}
+}
+	
---
+++ Assets/Scripts/InteractableSphere.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableSphere : InteractableObject {
+	
+	public override void DoGrab() {
+		Debug.Log ("GRAB!");
+	}
+
+	public override void DoSee() {
+		Debug.Log ("SEE!");
+	}
+
+	public override void DoPush() {
+		Debug.Log ("PUSH!");
+	}
+}
