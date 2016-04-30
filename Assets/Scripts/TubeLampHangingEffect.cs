10193317c7c9ca0a318488498f65ed90818d354d
Kensousen <dafede333@gmail.com>
2016-04-30 17:19:58 +0200

    MUCHAS COSAS

---
+++ Assets/Scripts/TubeLampHangingEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TubeLampHangingEffect : MonoBehaviour {
+
+	public GameObject tubeLamp;
+	Collider tubeCollider;
+
+	// Use this for initialization
+	void Start () {
+		tubeCollider=tubeLamp.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag=="Player")
+			tubeCollider.attachedRigidbody.useGravity = true;
+
+	}
+}
